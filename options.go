2d1997a15ee10e971f2a901d2c2ca7ba8dfcb3d3
sdn0303 <sdn0303.guitar@gmail.com>
2019-12-30 19:46:35 +0900

    add functional options.

--- options.go
+++ options.go
@@ -1,1 +1,17 @@
 package requests
+
+import "time"
+
+type Option func(*Requests)
+
+func MaxRetry(limit uint64) Option {
+	return func(requests *Requests) {
+		requests.RetryLimit = limit
+	}
+}
+
+func TimeOut(limit int) Option {
+	return func(requests *Requests) {
+		requests.HttpClient.Timeout = time.Duration(limit) * time.Second
+	}
+}
